fb15eb45c7783f8a94297bd6adaa75281137619e
rb8066 <rb8066@srmist.edu.in>
2025-09-08 06:41:42 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -1,49 +1,31 @@
 `default_nettype none
-`timescale 1ns / 1ps
+module tb_digital_logic_trainer;
 
-/* This testbench just instantiates the module and makes some convenient wires
-   that can be driven / tested by the cocotb test.py.
-*/
-module tb ();
+reg a, b;
+wire and_out, or_out, not_a, nand_out, nor_out, xor_out, xnor_out;
 
-  // Dump the signals to a VCD file. You can view it with gtkwave or surfer.
-  initial begin
-    $dumpfile("tb.vcd");
-    $dumpvars(0, tb);
-    #1;
-  end
+// Instantiate the design
+digital_logic_trainer uut (
+    .a(a), .b(b),
+    .and_out(and_out),
+    .or_out(or_out),
+    .not_a(not_a),
+    .nand_out(nand_out),
+    .nor_out(nor_out),
+    .xor_out(xor_out),
+    .xnor_out(xnor_out)
+);
 
-  // Wire up the inputs and outputs:
-  reg clk;
-  reg rst_n;
-  reg ena;
-  reg [7:0] ui_in;
-  reg [7:0] uio_in;
-  wire [7:0] uo_out;
-  wire [7:0] uio_out;
-  wire [7:0] uio_oe;
-`ifdef GL_TEST
-  wire VPWR = 1'b1;
-  wire VGND = 1'b0;
-`endif
-
-  // Replace tt_um_example with your module name:
-  tt_um_example user_project (
-
-      // Include power ports for the Gate Level test:
-`ifdef GL_TEST
-      .VPWR(VPWR),
-      .VGND(VGND),
-`endif
-
-      .ui_in  (ui_in),    // Dedicated inputs
-      .uo_out (uo_out),   // Dedicated outputs
-      .uio_in (uio_in),   // IOs: Input path
-      .uio_out(uio_out),  // IOs: Output path
-      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
-      .ena    (ena),      // enable - goes high when design is selected
-      .clk    (clk),      // clock
-      .rst_n  (rst_n)     // not reset
-  );
+initial begin
+    $display("A B | AND OR NOT_A NAND NOR XOR XNOR");
+    $display("--------------------------------------");
+    for (integer i = 0; i < 4; i = i + 1) begin
+        {a, b} = i;
+        #10; // wait for signal to propagate
+        $display("%b %b |  %b   %b    %b     %b    %b   %b    %b",
+                 a, b, and_out, or_out, not_a, nand_out, nor_out, xor_out, xnor_out);
+    end
+    $finish;
+end
 
 endmodule
